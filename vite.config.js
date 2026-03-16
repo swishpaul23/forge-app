0822fe9096c711adf4b923f1987a22504e867526
stuart <stuartwpaul23@gmail.com>
2026-03-15 18:33:58 -0700

    Created html landing page for users

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()] })
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main:    'index.html',      // your React app
+        landing: 'landing.html',   // the new page
+      }
+    }
+  }
+})
